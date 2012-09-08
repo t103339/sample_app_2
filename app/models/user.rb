13d35fece2474916eacf39efecdcbd88a397e6e3
Marv G. Niederhaus <mniederhas@aol.com>
2012-09-08 12:02:51 -0500

    change to email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   validates :name,  presence: true, 
   						  length: { maximum: 50 }
   
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
   					uniqueness: {case_sensitive: false},
   					format: { with: VALID_EMAIL_REGEX }
